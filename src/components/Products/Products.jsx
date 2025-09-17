4e22dceb6efe71d2db4d426770d594c2e78117aa
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-17 20:21:08 +0600

    alert button

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -8,6 +8,7 @@ const Products = (props) => {
     <div>
       <h1>Products Name: {props.product.name}</h1>
       <h2>Products Price: {props.product.price}</h2>
+      <button>Click for Alert</button>
     </div>
   )
 }
