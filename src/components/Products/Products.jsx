ae6534298975529f9697bf57b9ef9b6ff88c0042
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-17 20:24:08 +0600

    handleAlert function created in products components

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,11 +4,15 @@ import './Products.css'
 const Products = (props) => {
     console.log(props.product)
 
+    const handleAlert = () => {
+        alert('Alert button clicked!');
+    }
+
   return (
     <div>
       <h1>Products Name: {props.product.name}</h1>
       <h2>Products Price: {props.product.price}</h2>
-      <button>Click for Alert</button>
+      <button onClick={handleAlert}>Click for Alert</button>
     </div>
   )
 }
