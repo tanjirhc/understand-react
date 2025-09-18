4dbb5a936df18bb89418d4a5bf32e71bd4fce6d4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 20:28:52 +0600

    array showing

--- src/components/Singleuserdata/Singleuserdata.jsx
+++ src/components/Singleuserdata/Singleuserdata.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const Singleuserdata = ({singleData}) => {
-    const {name, website, phone} = singleData;
+    const {name, website, phone, company} = singleData;
+    console.log(singleData)
 
   return (
     <div>
       <h1>{name}</h1>
       <p>{website}</p>
       <h3>{phone}</h3>
+      <h4>Company Name: {company.name}</h4>
     </div>
   )
 }
