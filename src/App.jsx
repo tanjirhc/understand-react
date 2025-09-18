1981c6042495cd9bf79ec48ff028891c214ace15
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 19:40:52 +0600

    API data fetching using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 
 
-import { useEffect, useState } from 'react'
 import './App.css'
 
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState('This is Tanjir Hossain ');
 
-  useEffect(() => {
-    console.log('When Data Loaded')
-  }, [name])
-
-  const updateCounter = () => {
-    const newName = name + 'Coding Addict';
-    setName(newName);
-  }
-  
   return (
     <div>
-      <h1>Name: {name}</h1>      
-      <button onClick={updateCounter}>Steps</button>
-      <h1>Steps: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Update Steps</button>
+      <h1>Hello API Fetching</h1>
     </div>
   )
 }
