35f2525e278d7f49fed2cbab0e050179a28398ab
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 18:09:41 +0600

    decrese useState

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,17 @@ function App() {
     const newPrice = price + 1;
     setPrice(newPrice);
   }
+
+  const decreaseHandle = () => {
+    const newPrice = price - 1;
+    setPrice(newPrice);
+  }
   
   return (
     <div>
       <h1>Price : {price}</h1>
       <button onClick={increaseHandle}>Increase</button> &nbsp; &nbsp; &nbsp;
-      <button>Decrease</button>
+      <button onClick={decreaseHandle}>Decrease</button>
 
       <h2>Price: {price}</h2>
       <p>Price: {price}</p>
