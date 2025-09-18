458305544801b1cb067ddd9b392ee85c28c11222
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 17:31:47 +0600

    &nbsp space

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import './App.css'
 
 function App() {
 
-  
+  const price = 100;
 
   return (
     <div>
-      <h1>Undertstandin useState Hook</h1>
+      <h1>Price : 0</h1>
+      <button>Increase</button> &nbsp;
+      <button>Decrease</button>
+      <h2>Price: 0</h2>
+      <p>Price: 0</p>
+      <h4>Price: 0</h4>
     </div>
   )
 }
