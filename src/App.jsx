ff6d9bfc3c883e446c68993b3eb82916f0cc8345
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 18:12:48 +0600

    useEffect Hook Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 
-import { useState } from 'react';
+
 import './App.css'
 
 
 
 function App() {
 
-  const [price, setPrice] = useState(10);
 
-
-  const increaseHandle = () => {
-    const newPrice = price + 1;
-    setPrice(newPrice);
-  }
-
-  const decreaseHandle = () => {
-    const newPrice = price - 1;
-    setPrice(newPrice);
-  }
   
   return (
     <div>
-      <h1>Price : {price}</h1>
-      <button onClick={increaseHandle}>Increase</button> &nbsp; &nbsp; &nbsp;
-      <button onClick={decreaseHandle}>Decrease</button>
-
-      <h2>Price: {price}</h2>
-      <p>Price: {price}</p>
-      <h4>Price: {price}</h4>
+      <h1>useEffect Hook</h1>
     </div>
   )
 }
