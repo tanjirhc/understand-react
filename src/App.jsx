ae01cc5399aae62600d24763476149d0b1e4263d
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 18:28:31 +0600

    steps count increased by useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 
 
 function App() {
+  const [count, setCount] = useState(0);
 
 
   useEffect(() => {
     alert('useEffect called')
   }, [])
+
+  const updateCounter = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
   
   return (
     <div>
-      <h1>useEffect Hook</h1>
+      <h1>Steps: {count}</h1>
+      <button onClick={updateCounter}>Steps</button>
     </div>
   )
 }
