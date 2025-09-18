6edbb5503f1f94037d053ede77b07d38027964de
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 17:29:12 +0600

    useState Hook Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
 
 import './App.css'
-import Products from './components/Products/Products';
+
 
 
 function App() {
 
-  const products = [ 
-    {name: "Iphone 14 Pro Max", price: "1,29,900"},
-    {name: "Samsung Galaxy S23 Ultra", price: "1,05,999"},
-    {name: "OnePlus 11R", price: "39,999"},
-    {name: "Google Pixel 7 Pro", price: "84,999"},
-    {name: "Xiaomi 13 Pro", price: "57,999"},
-    {name: "Sony Xperia 1 IV", price: "1,19,990"},
-    {name: "Motorola Edge 30 Ultra", price: "49,999"}
-   ];
-
-   const handleAlert = () => {
-        alert('Alert button clicked!');
-    }
+  
 
   return (
     <div>
-      {
-        products.map(product => <Products 
-          product = {product}
-          alertFunction = {handleAlert}
-        ></Products>)
-      }
+      <h1>Undertstandin useState Hook</h1>
     </div>
   )
 }
