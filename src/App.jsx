003d4e0691e9289b780b89fd9a114cd36869f927
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-18 19:48:44 +0600

    array fetch from fake API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
 
+import { useEffect } from 'react'
 import './App.css'
 
 
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error fetching data:', error))
+  }, [])
 
   return (
     <div>
