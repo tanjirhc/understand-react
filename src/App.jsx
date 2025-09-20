8a57c9a3262f06c6ad12d019002829e4e896ef02
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-20 18:39:09 +0600

    fakeData showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [fakeData, setFakeData] = useState([]);
+  console.log(fakeData)
 
   useEffect(() => {
     fetch('fakeData.json')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setFakeData(data))
   }, [])
 
   return (
