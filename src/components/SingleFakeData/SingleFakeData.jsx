e2cc6a82b0919c13b70f4b162f25dc426369a158
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-20 19:11:27 +0600

    showing img

--- src/components/SingleFakeData/SingleFakeData.jsx
+++ src/components/SingleFakeData/SingleFakeData.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const SingleFakeData = ({singleData}) => {
-     const {name,age} = singleData;
+     const {name,age,profession, img} = singleData;
   return (
     <div>
       <h2>Name: {name}</h2>
       <p>Age: {age}</p>
+      <h3>Profession: {profession}</h3>
+      <img src={img} alt="This is Fake Img" />
     </div>
   )
 }
