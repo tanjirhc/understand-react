4209fe818e1b0130ce1a6f043e28a5282f7dc2df
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-20 17:24:19 +0600

    tailwind css setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 
-
-import { useEffect, useState } from 'react'
 import './App.css'
-import Singleuserdata from './components/Singleuserdata/Singleuserdata';
-
-
 
 function App() {
-  const [allData, setAllData] = useState([])
 
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setAllData(data))
-  }, [])
 
   return (
-    <div className='grid grid-cols-3 gap-4 p-5'>      
-      {
-        allData.map(singleData => <Singleuserdata
-          key={singleData.id} 
-          singleData={singleData}
-          />)
-      }
+    <div >      
+      Hello
     </div>
   )
 }
