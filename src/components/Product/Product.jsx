b4688e7d11fc5f4dcc81a47427b01d6294d9c1d0
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-21 16:59:18 +0600

    props data shown in UI

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Product = ({product}) => {
+    const {name, age, profession} = product;
   return (
     <div>
-      <h2>Single Product</h2>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+      <p>{profession}</p>
     </div>
   )
 }
