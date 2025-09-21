fd6019c2d98eaab61782b29ab1277ff5b2c4b67d
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-21 18:01:27 +0600

    singleCart Data show in console

--- src/components/Placement/Placement.jsx
+++ src/components/Placement/Placement.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import SingleCartData from '../SingleCartData/SingleCartData';
 
 const Placement = ({cart}) => {
-    console.log(cart);
+
   return (
     <div>
       <h2 className='text-2xl'>Placement</h2>
       {
-        cart.map(singleData => <h2>{singleData.name}</h2>)
+        cart.map(singleData => <SingleCartData
+            key={singleData.id} 
+            singleData={singleData}
+        />)
       }
     </div>
   )
---
+++ src/components/SingleCartData/SingleCartData.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SingleCartData = ({singleData}) => {
+    console.log(singleData)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SingleCartData
