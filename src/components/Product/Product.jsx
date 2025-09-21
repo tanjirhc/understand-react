216a1b20b9719979fff5ecbbe02f24d66145f9dc
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-21 14:20:13 +0600

    anonymous function

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const Product = ({product, addToCart}) => {
    const {name, age, profession} = product;
-   console.log(product)
 
   return (
     <div className='border-2 m-4 p-4 border-cyan-600 rounded-lg align-center'>
       <h2 className='text-2xl'>{name}</h2>
       <p>{age}</p>
       <p>{profession}</p>
-      <button onClick={addToCart} className='bg-red-600 px-4 py-3 mt-2'>Add to Cart</button>
+      <button onClick={() => addToCart(product)} className='bg-red-600 px-4 py-3 mt-2'>Add to Cart</button>
     </div>
   )
 }
