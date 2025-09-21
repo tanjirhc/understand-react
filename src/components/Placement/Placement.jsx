e50acb4b6188b03c5a054a328460f2e1474c5307
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-21 14:33:05 +0600

    sending single cart by id

--- src/components/Placement/Placement.jsx
+++ src/components/Placement/Placement.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import SingleCart from '../SingleCart/SingleCart'
 
 const Placement = ({cart}) => {
+    console.log(cart)
   return (
     <div>
-      Placement
+      <h2 className='text-3xl'>Placement</h2>
+      {
+        cart.map(singleCart =><SingleCart
+            key={singleCart.id}
+            singleCart={singleCart} 
+            /> )
+      }
     </div>
   )
 }
