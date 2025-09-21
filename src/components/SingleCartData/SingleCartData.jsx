1d9f9f984833c4386ae27777163c5d86cf0dcb5a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-21 18:03:36 +0600

    array destructuring

--- src/components/SingleCartData/SingleCartData.jsx
+++ src/components/SingleCartData/SingleCartData.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const SingleCartData = ({singleData}) => {
-    console.log(singleData)
+    const {name, age, profession} = singleData;
   return (
     <div>
-      
+      <h2>{name}</h2>
+        <h2>{age}</h2>
+        <p>{profession}</p>
+        <hr />
     </div>
   )
 }
