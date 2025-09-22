bb5cd16beb145e23b6130ccdedd329276aa05141
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-22 17:47:11 +0600

    react router routing

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,16 @@ const router = createBrowserRouter([
     element: <h1>Hello "/"</h1>,
   },
   {
-    path: "/about us  ",
+    path: "/about",
     element: <h1>I am  About Path</h1>,
+  },
+  {
+    path: "/contact",
+    element: <h1>I am Contact Path</h1>,
+  },
+  {
+    path: "/blog",
+    element: <h1>I am blog route</h1>
   }
 ]);
 
