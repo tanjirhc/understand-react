407a6de11764a0cf78e0bc4896619f664da0fee6
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-22 17:22:00 +0600

    npm i react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 
-import { useState } from 'react'
 import './App.css'
-import Products from './components/Products/Products'
-import Placement from './components/Placement/Placement'
-  
+
 
 function App() {
-  const [cart, setCart] = useState([]);
-
-  const addToCartData = (product) => {
-    const newCart = [...cart, product];
-    setCart(newCart);
-    console.log(newCart);
-  }
-  
+ 
   return (
-    <div className='flex gapx-6'>      
-      <Products addToCartData = {addToCartData} />
-      <Placement cart ={cart}/>
+    <div >      
+      Hello
     </div>
   )
 }
